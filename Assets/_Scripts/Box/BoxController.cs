79d5bfa3edb2ea644d1a97cb377735056056da8f
kttanh <anhktthe176302@fpt.edu.vn>
2025-03-19 21:54:43 +0700

    add spawn box when box has been destroyed

--- Assets/_Scripts/Box/BoxController.cs
+++ Assets/_Scripts/Box/BoxController.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ public class BoxController : MonoBehaviour
 {
     [SerializeField] List<GameObject> Items;
     public float dropForce = 0.5f;
+    public static event Action OnBoxDestroyed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -27,12 +29,13 @@ public class BoxController : MonoBehaviour
             Vector2 boxPosition = transform.position;
             Destroy(gameObject);
             SpawnRandomItem(boxPosition);
+            OnBoxDestroyed?.Invoke();
         }
     }
 
     private void SpawnRandomItem(Vector2 position)
     {
-        int ramdomIndex = Random.Range(0, Items.Count);
+        int ramdomIndex = UnityEngine.Random.Range(0, Items.Count);
         //SpawnerManager.Instance.SpawnObject(Items[ramdomIndex], position, Quaternion.identity);
 
         GameObject spawnedItem = Instantiate(Items[ramdomIndex], position, Items[ramdomIndex].transform.rotation);
@@ -40,9 +43,9 @@ public class BoxController : MonoBehaviour
         rb.freezeRotation = true;
         if (rb != null)
         {
-            Vector2 launchDirection = Random.insideUnitCircle.normalized * dropForce;
+            Vector2 launchDirection = UnityEngine.Random.insideUnitCircle.normalized * dropForce;
             rb.linearVelocity = launchDirection;
-            rb.AddTorque(Random.Range(-3f, 3f), ForceMode2D.Impulse);
+            rb.AddTorque(UnityEngine.Random.Range(-3f, 3f), ForceMode2D.Impulse);
         }
     }
 }
