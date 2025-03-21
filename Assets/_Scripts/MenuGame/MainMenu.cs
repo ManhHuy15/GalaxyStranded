2f13a45033dff5db20573cd3524b6e986e4f26e1
Le Duy Hai <haileduy72@gmail.com>
2025-03-21 07:42:55 +0700

    done UI

---
+++ Assets/_Scripts/MenuGame/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        EditorApplication.isPlaying = false;
+    }
+}
